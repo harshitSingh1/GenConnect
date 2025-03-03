c8478b7bc380fc44606dc3db9c4b1c560eb72f8f
Harshit Singh <95095505+harshitSingh1@users.noreply.github.com>
2025-03-03 19:44:41 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+// backend\index.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./Routes/AuthRouter');
+const ProductRouter = require('./Routes/ProductRouter');
+
+require('dotenv').config();
+require('./Models/db');
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+
+app.options("*", cors());
+
+app.use(express.json({ limit: "10mb" })); // Increase payload limit
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(bodyParser.json());
+
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
