50ce3f799b21d65190a1997513a6522234f2c4d3
Harshit Singh <sharshitsingh007@gmail.com>
2025-03-04 21:19:51 +0530

    Initial commit: Added frontend and backend

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,6 +1,6 @@
 // backend\Models\db.js
 const mongoose = require('mongoose');
-const mongo_url = process.env.MONGODB_URI;
+const mongo_url = process.env.MONGO_URI;
 mongoose.connect(mongo_url)
 .then(() => {
     console.log('Connected to MongoDB')
