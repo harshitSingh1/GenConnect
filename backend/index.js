4ca3d252fb33963484970a899ffa64877585ddd6
Harshit Singh <95095505+harshitSingh1@users.noreply.github.com>
2025-03-05 11:32:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,9 @@
-// backend\index.js
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const AuthRouter = require('./Routes/AuthRouter');
-const ProductRouter = require('./Routes/ProductRouter');
-const serverless = require("serverless-http");
+const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 8080;
 
-require('dotenv').config();
-require('./Models/db');
-
-const PORT = process.env.PORT || 8080;
-
-app.get('/ping', (req, res) => {
-    res.send('pong');
+// Start the server
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-
-app.options("*", cors());
-
-app.use(express.json({ limit: "10mb" })); // Increase payload limit
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(bodyParser.json());
-
-app.use('/auth', AuthRouter);
-app.use('/products', ProductRouter);
-
-module.exports = app;
-module.exports.handler = serverless(app);
-
