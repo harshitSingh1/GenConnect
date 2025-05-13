978abbf87caa00c33d13a37977de3f85b7b4da43
Harshit Singh <95095505+harshitSingh1@users.noreply.github.com>
2025-05-13 17:26:54 +0530

    Delete backend directory

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('./app');
-const port = process.env.PORT || 8080;
-
-// Start the server
-const server = http.createServer(app);
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
